2df2229b44f89ffe7698b626c0435de0797de67f
Xalune <aidar2102003@gmail.com>
2023-06-01 15:10:16 +0500

    feat: adding a notification for a blocked user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,30 +20,22 @@ const Home = () => {
   const currentUser = currentUsers.find(
     (user) => user.id === localStorage.token
   );
-  const isAdmin = currentUser && currentUser.role === 'admin' ? true : false;
+  const isAdmin = currentUser && currentUser.role === "admin" ? true : false;
+  const isBlocked =
+    currentUser && currentUser.role === "blocked" ? true : false;
   return (
     <>
       <Header></Header>
-      <div className="d-flex flex-column align-items-center">
-        <div className="home-page-head">
-          <h1 className="events-head text-center mb-4 mt-3">
-            {headContent}
-            <Button
-              className="event-settings"
-              variant="light"
-              onClick={() => setModalShow(true)}
-            ></Button>
-          </h1>
-        </div>
-        {!localStorage.firstname ? (
+      {isBlocked ? (
+        <div className="d-flex flex-column align-items-center">
           <div
-            className="alert alert-info alert-dismissible fade in show mb-4"
+            className="alert alert-info alert-dismissible fade in show mb-4 mt-4"
             role="alert"
-            style={{maxWidth: "1000px"}}
+            style={{ maxWidth: "800px" }}
           >
-            <strong>Уважаемый пользователь!</strong> Для просмотра полной
-            информации о мероприятиях, а также для участия в них необходимо
-            зарегистрироваться или авторизоваться.
+            <strong>Уважаемый пользователь!</strong> Администратор сайта был
+            вынужден заблокировать Ваш аккаунт в связи с нарушением правил
+            нашей платформы.
             <button
               type="button"
               className="btn-close"
@@ -60,19 +52,62 @@ const Home = () => {
               }}
             ></button>
           </div>
-        ) : (
-          <></>
-        )}
-        <div className="d-flex flex-column align-items-center gap-5 pb-5">
-          {localStorage.firstname ? (
-            isAdmin ? <AdminEvents></AdminEvents> : <Events></Events>
-          ) : (
-            <HiddenEvents></HiddenEvents>
-          )}
         </div>
+      ) : (
+        <div className="d-flex flex-column align-items-center">
+          <div className="home-page-head">
+            <h1 className="events-head text-center mb-4 mt-3">
+              {headContent}
+              <Button
+                className="event-settings"
+                variant="light"
+                onClick={() => setModalShow(true)}
+              ></Button>
+            </h1>
+          </div>
+          {!localStorage.firstname ? (
+            <div
+              className="alert alert-info alert-dismissible fade in show mb-4"
+              role="alert"
+              style={{ maxWidth: "1000px" }}
+            >
+              <strong>Уважаемый пользователь!</strong> Для просмотра полной
+              информации о мероприятиях, а также для участия в них необходимо
+              зарегистрироваться или авторизоваться.
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="alert"
+                data-dismiss="alert"
+                aria-label="Close"
+                onClick={() => {
+                  const alert = document.querySelector(".alert");
+                  console.log(alert);
+                  alert.classList.remove("show");
+                  setTimeout(() => {
+                    alert.remove();
+                  }, "300");
+                }}
+              ></button>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="d-flex flex-column align-items-center gap-5 pb-5">
+            {localStorage.firstname ? (
+              isAdmin ? (
+                <AdminEvents></AdminEvents>
+              ) : (
+                <Events></Events>
+              )
+            ) : (
+              <HiddenEvents></HiddenEvents>
+            )}
+          </div>
 
-        <ModalSettings show={modalShow} onHide={() => setModalShow(false)} />
-      </div>
+          <ModalSettings show={modalShow} onHide={() => setModalShow(false)} />
+        </div>
+      )}
     </>
   );
 };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,25 +2,63 @@ import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import MyEvents from "../components/MyEvents";
 import EventForm from "../components/EventForm";
+import { useSelector } from "react-redux";
 
 const Home = () => {
   const [modalShow, setModalShow] = useState(false);
+  const currentUsers = useSelector((state) => state.users.users);
+  const currentUser = currentUsers.find(
+    (user) => user.id === localStorage.token
+  );
+  const isBlocked =
+    currentUser && currentUser.role === "blocked" ? true : false;
   return (
     <>
       <Header />
-      <h1 className="text-center mt-3">Мои мероприятия</h1>
-      <div className="d-flex flex-column align-items-center gap-5 pb-5">
-        <button
-          type="button"
-          className="btn-create-event btn btn-outline-success btn-lg btn-block mt-4"
-          onClick={() => setModalShow(true)}
-        >
-          Создать мероприятие
-        </button>
-        <EventForm show={modalShow} onHide={() => setModalShow(false)} />
-        <MyEvents />
-      </div>
-      <div className="pb-5"></div>
+      {isBlocked ? (
+        <div className="d-flex flex-column align-items-center">
+          <div
+            className="alert alert-info alert-dismissible fade in show mb-4 mt-4"
+            role="alert"
+            style={{ maxWidth: "800px" }}
+          >
+            <strong>Уважаемый пользователь!</strong> Администратор сайта был
+            вынужден заблокировать Ваш аккаунт в связи с нарушением правил нашей
+            платформы.
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="alert"
+              data-dismiss="alert"
+              aria-label="Close"
+              onClick={() => {
+                const alert = document.querySelector(".alert");
+                console.log(alert);
+                alert.classList.remove("show");
+                setTimeout(() => {
+                  alert.remove();
+                }, "300");
+              }}
+            ></button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h1 className="text-center mt-3">Мои мероприятия</h1>
+          <div className="d-flex flex-column align-items-center gap-5 pb-5">
+            <button
+              type="button"
+              className="btn-create-event btn btn-outline-success btn-lg btn-block mt-4"
+              onClick={() => setModalShow(true)}
+            >
+              Создать мероприятие
+            </button>
+            <EventForm show={modalShow} onHide={() => setModalShow(false)} />
+            <MyEvents />
+          </div>
+          <div className="pb-5"></div>
+        </>
+      )}
     </>
   );
 };
